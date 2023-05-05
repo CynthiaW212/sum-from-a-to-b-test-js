a3acb3a556216cbe1292a591998cc1e57ea917a6
Yuze Wang <yuzewang621@gmail.com>
2023-05-05 04:18:35 -0600

    use recursion solve sum up

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
-function sum(fromN, toN) {
+const sum = function(fromN, toN) {
   // Sum all the values from fromN up to toN
-}
+  if (fromN > toN) {
+    return 0;
+  }
+  return sum(fromN + 1, toN) + fromN;
+
+};
+console.log(sum(3, 7));
 
 module.exports = sum;
